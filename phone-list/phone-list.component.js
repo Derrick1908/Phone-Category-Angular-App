8b40a053a2da218c9728bb74f7915f7164625965
Derrick1908 <derekvenom@gmail.com>
2021-07-18 18:44:37 +0530

    Refactoring of Code for Debugging Purposes

--- phone-list/phone-list.component.js
+++ phone-list/phone-list.component.js
@@ -2,15 +2,9 @@
 
 //Register `phoneList` component, along with its associated controller and template
 angular.
-    module('phonecatApp').
+    module('phoneList').
     component('phoneList',{ //This is the name what AngularJS uses to match to the `<phone-list>` element.
-        template:
-        '<ul>' +
-            '<li ng-repeat="phone in $ctrl.phones">' +
-                '<span>{{phone.name}}</span>' +
-                '<p>{{phone.snippet}}</p>' +
-            '</li>' +
-        '</ul>',
+        templateUrl: 'phone-list/phone-list.template.html',
         controller: function PhoneListController() {
             this.phones = [
                 {
